fcf0f10f5c6247d9042a99d28847411258f8912d
zh1668118834 <1668118834@qq.com>
2022-12-23 14:49:50 +0800

    init

---
+++ Assets/Runtime/Entry/Entry.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entry : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+}
